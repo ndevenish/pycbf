75e5b89909aa9e0f6a15904bca07bd1d1b24f63b
Nicholas Devenish <ndevenish@gmail.com>
2021-05-05 20:43:25 +0100

    Build full pycbf import

--- build.py
+++ build.py
@@ -6,10 +6,47 @@ from typing import Any, Dict
 # import skbuild
 # import skbuild.constants
 
+CBF_SOURCES = [
+    "cbflib/src/cbf.c",
+    "cbflib/src/cbf_airy_disk.c",
+    "cbflib/src/cbf_alloc.c",
+    "cbflib/src/cbf_ascii.c",
+    "cbflib/src/cbf_binary.c",
+    "cbflib/src/cbf_byte_offset.c",
+    "cbflib/src/cbf_canonical.c",
+    "cbflib/src/cbf_codes.c",
+    "cbflib/src/cbf_compress.c",
+    "cbflib/src/cbf_context.c",
+    "cbflib/src/cbf_copy.c",
+    "cbflib/src/cbf_file.c",
+    "cbflib/src/cbf_getopt.c",
+    "cbflib/src/cbf_lex.c",
+    "cbflib/src/cbf_minicbf_header.c",
+    "cbflib/src/cbf_nibble_offset.c",
+    "cbflib/src/cbf_packed.c",
+    "cbflib/src/cbf_predictor.c",
+    "cbflib/src/cbf_read_binary.c",
+    "cbflib/src/cbf_read_mime.c",
+    "cbflib/src/cbf_simple.c",
+    "cbflib/src/cbf_string.c",
+    "cbflib/src/cbf_stx.c",
+    "cbflib/src/cbf_tree.c",
+    "cbflib/src/cbf_ulp.c",
+    "cbflib/src/cbf_uncompressed.c",
+    "cbflib/src/cbf_write.c",
+    "cbflib/src/cbf_write_binary.c",
+    "cbflib/src/cbf_ws.c",
+    "cbflib/src/cbff.c",
+    "cbflib/src/fgetln.c",
+    "cbflib/src/img.c",
+    "cbflib/src/md5c.c",
+    # "cbflib/src/cbf_hdf5.c",
+    # "cbflib/src/cbf_hdf5_filter.c",
+]
 extensions = [
     Extension(
         "pycbf._pycbf",
-        sources=["pycbf_wrap.c"],
+        sources=["pycbf_wrap.c", *CBF_SOURCES],
         include_dirs=[str(PurePath(__file__).parent / "cbflib" / "include")],
     )
 ]
