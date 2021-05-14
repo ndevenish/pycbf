3c03828ed77513483863e8c0d40f4f8122149e70
Nicholas Devenish <ndevenish@gmail.com>
2021-05-14 15:54:12 +0100

    Add read_buffer interface directly to pycbf

--- tests/test_2.py
+++ tests/test_2.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 
+import pytest
+
 import pycbf
 
 DATA_DIR = Path(__file__).parent.parent / "data"
@@ -13,3 +15,16 @@ def test_2():
     print(("Rotation axis is", g.get_rotation_axis()))
     d = obj.construct_detector(0)
     print(("Beam center is", d.get_beam_center()))
+
+
+def test_2_buffered():
+    cbf = pycbf.cbf_handle_struct()
+    buffer = (DATA_DIR / "adscconverted.cbf").read_bytes()
+    cbf.read_buffer(buffer)
+    cbf.select_datablock(0)
+    g = cbf.construct_goniometer()
+    print(("Rotation axis is", g.get_rotation_axis()))
+    assert g.get_rotation_axis() == pytest.approx([1, 0, 0])
+    d = cbf.construct_detector(0)
+    print(("Beam center is", d.get_beam_center()))
+    assert d.get_beam_center() == pytest.approx([1535.5, 1535.5, 157.52387, -157.52387])
